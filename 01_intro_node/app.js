fcf76cbb9da81c7d6fe43ce534871f8622b0d320
César Alves <ceaser.alves03@gmail.com>
2025-06-25 23:54:23 -0300

    Intro ao Node Js

---
+++ 01_intro_node/app.js
@@ -0,0 +1,40 @@
+
+var  calculadora = require("./calculadora")
+
+
+ var result = calculadora.mult(10,20)
+
+
+console.log(result)
+
+
+
+console.log(calculadora.nome)
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com"
+
+console.log("Hello World!")
+console.log("Meu nome é César")
+console.log("E eu estou aprendendo Node. js com o guia do programador")
+
+
+if(mostrarSite){ // Se mostrar site
+    console.log(site) // // Printe  o site
+}
+*/
+
+
---
+++ 01_intro_node/calculadora.js
@@ -0,0 +1,27 @@
+
+var nome = "Minha Calculadora V1";
+
+function soma(a,b){
+  return  a + b
+}
+
+function  mult(a,b){
+  return  a * b
+}
+
+function sub(a,b){
+  return  a - b
+}
+
+function div(a,b){
+  return  a / b
+}
+
+
+module.exports = {
+    soma,
+    sub,
+    mult,
+    div,
+    nome
+};
